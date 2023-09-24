21325627271d5ceb7c2f7c3bc8980aa1b72647ad
Md. Mohiuddin <mohiuddin.ice.ru@gmail.com>
2023-09-24 19:25:18 +0600

    included login route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,5 +8,9 @@ router.post("/register", (req, res) => {
     authController.register(req, res)
 })
 
+router.post("/login", (req, res) => {
+    authController.login(req, res)
+})
+
 module.exports = router
 
