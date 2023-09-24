4e8020fac58c088b93d1dc65be39c369282b7694
Md. Mohiuddin <mohiuddin.ice.ru@gmail.com>
2023-09-24 20:39:59 +0600

    middleware Added

---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+
+function verifyToken(req, res, next) {
+    const token =
+        req.body.token || req.query.token || request.headers["x-access-token"];
+    if (!token) {
+        throw res.status(403).json({
+            success: false,
+            message: "Token required for authentication",
+        });
+    }
+    try {
+        const decoded = jwt.verify(token, process.env.SECRET_KEY);
+        req.user = decoded;
+    } catch (e) {
+        console.log(e);
+        return res
+            .status(401)
+            .json({ success: false, message: "Invalid token" });
+    }
+}
+
+module.exports = verifyToken;
